0369db99244f94db283fda3f9149f708c05b11d3
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2015-05-05 15:50:17 -0700

    Add a Settings button and activity.

--- src/main/java/io/ibrahim/wearmirror/SettingsActivity.java
+++ src/main/java/io/ibrahim/wearmirror/SettingsActivity.java
@@ -6,21 +6,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
-import android.os.
-public class MainActivity extends Activity
+
+public class SettingsActivity extends Activity
 {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.settings);
         // Button b = (Button)findViewById(R.id.settings_button);
-    }
-
-    public void openSettings(View settingsButton){
-        Log.d("WearMirror", "Clicked button");
-        Intent intent = new Intent(this, SettingsActivity.class);
-        startActivity(intent);
     }
 }
